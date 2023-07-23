abacc8d609d083e2a8b17dbdaa7eda3758de7806
Raushanray <raushankuray1998@gmail.com>
2023-07-23 17:12:48 +0530

    create a service registry project and registr both service payment and order service with service registry..

--- order-service/src/main/java/com/raushan/os/api/OrderServiceApplication.java
+++ order-service/src/main/java/com/raushan/os/api/OrderServiceApplication.java
@@ -2,10 +2,13 @@ package com.raushan.os.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
@@ -14,6 +17,7 @@ public class OrderServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}

e10bee0fcaf08ce3c92026924bd62fbc61b465d8
Raushanray <raushankuray1998@gmail.com>
2023-07-24 12:57:16 +0530

    Here I can integrate cloud api gateway in our application to act as a middle layar before access in your microservice..

--- payment-service/src/main/java/com/raushan/ps/api/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/raushan/ps/api/repository/PaymentRepository.java
@@ -4,4 +4,5 @@ import com.raushan.ps.api.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    Payment findByOrderId(int orderId);
 }

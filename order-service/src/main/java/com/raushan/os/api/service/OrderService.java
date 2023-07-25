36526fda156010f229f4eaf42d167fcd966cc8ba
Raushanray <raushankuray1998@gmail.com>
2023-07-25 11:08:22 +0530

    configure all service with config server application..and also create a git repository and add all the configuration..

--- order-service/src/main/java/com/raushan/os/api/service/OrderService.java
+++ order-service/src/main/java/com/raushan/os/api/service/OrderService.java
@@ -6,16 +6,24 @@ import com.raushan.os.api.common.TransactionResponse;
 import com.raushan.os.api.entity.Order;
 import com.raushan.os.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private RestTemplate template;
     @Autowired
+    @Lazy
     private OrderRepository repository;
+
+    @Value("${microservice.payment-service.endpoints.uri}")
+    private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request){
         String respose="";
@@ -24,7 +32,7 @@ public class OrderService {
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         //rest call
-       Payment paymentResponse =  template.postForObject("http://PAYMENT-SERVICE/payment/doPayment",payment,Payment.class);
+       Payment paymentResponse =  template.postForObject(ENDPOINT_URL,payment,Payment.class);
         respose= paymentResponse.getPaymentStatus().equals("success")?
                 "payment processing successful and order placed":
                 "there is a failure in payment api , order added to cart";
